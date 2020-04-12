59597653b65462d42c142dfba8b9871f2b7d13e0
faridibin <faridibin@gmail.com>
2020-04-12 19:37:58 -0400

    Revert "webpack"
    This reverts commit 894f756ea43ab210b5eaeb76eb1db994f0ed6a01.

--- config/auth.php
+++ config/auth.php
@@ -42,8 +42,9 @@ return [
         ],
 
         'api' => [
-            'driver' => 'passport',
-            'provider' => 'users'
+            'driver' => 'token',
+            'provider' => 'users',
+            'hash' => false,
         ],
     ],
 
@@ -67,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\User::class,
         ],
 
         // 'users' => [

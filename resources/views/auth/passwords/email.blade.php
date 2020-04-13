d843dd1bcffb8d13eb9b4e722de7cf049d7d5331
faridibin <faridibin@gmail.com>
2020-04-12 20:16:11 -0400

    setting up project files

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -42,6 +42,20 @@
                 </div>
             </div>
         </div>
+    </div>
+</div> --}}
+<div class="col-xl-5 col-lg-6 col-md-7">
+    <div class="text-center">
+        <h1 class="h2">Forgot password &#x1f62B;</h1>
+        <p class="lead">Enter your email address to reset</p>
+        <form>
+            <div class="form-group">
+                <input class="form-control" type="email" placeholder="Email Address" name="forgot-password-email" />
+            </div>
+            <button class="btn btn-block btn-primary" role="button" type="submit">
+            Send reset link
+            </button>
+        </form>
     </div>
 </div>
 @endsection

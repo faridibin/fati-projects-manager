7b33fa83853f6a332a3b75e90449b0b4bd260e1a
Farid Adam <faridibin@gmail.com>
2022-01-11 01:07:42 -0500

    Update 2020_04_13_002121_create_tasks_table.php

--- database/migrations/2020_04_13_002121_create_tasks_table.php
+++ database/migrations/2020_04_13_002121_create_tasks_table.php
@@ -15,7 +15,18 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('description');
+            $table->dateTime('start_date')->nullable();
+            $table->dateTime('end_date')->nullable();
+            $table->string('kanban_column')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('project_id');
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('project_id')->references('id')->on('projects');
         });
     }
 

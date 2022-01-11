e91771d8123a2679dc5773b5f377234a3e7758a6
Farid Adam <faridibin@gmail.com>
2022-01-11 01:07:36 -0500

    Update 2020_04_13_002108_create_projects_table.php

--- database/migrations/2020_04_13_002108_create_projects_table.php
+++ database/migrations/2020_04_13_002108_create_projects_table.php
@@ -15,7 +15,17 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('description');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->json('kanban_columns')->nullable();
+            $table->unsignedBigInteger('created_by');
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('created_by')->references('id')->on('users');
         });
     }
 

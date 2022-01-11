5991616b1e6b3a6df5cbf9252b524ee98dd1fdea
Farid Adam <faridibin@gmail.com>
2022-01-11 01:07:31 -0500

    Update 2020_04_13_001852_create_organizations_table.php

--- database/migrations/2020_04_13_001852_create_organizations_table.php
+++ database/migrations/2020_04_13_001852_create_organizations_table.php
@@ -15,7 +15,19 @@ class CreateOrganizationsTable extends Migration
     {
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->string('slug')->unique()->nullable();
+            $table->longText('description')->nullable();
+            $table->string('email')->nullable();
+            $table->string('billing_email')->nullable();
+            $table->string('gravatar_email')->nullable();
+            $table->string('url')->nullable();
+            $table->string('location')->nullable();
+            $table->unsignedBigInteger('created_by');
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('created_by')->references('id')->on('users');
         });
     }
 

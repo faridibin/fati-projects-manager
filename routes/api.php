2cf0857249edc2679dd7b7f0e98579607597bd97
Farid Adam <faridibin@gmail.com>
2022-01-16 12:07:17 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', 'Auth\LoginController@login')->name('login');
+    Route::post('register', 'Auth\RegisterController@register')->name('register');
+    Route::post('verify', 'Auth\VerificationController@resend')->name('verify');
+    Route::prefix('password')->name('password.')->group(function () {
+        Route::post('email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('email');
+        Route::post('reset', 'Auth\ResetPasswordController@reset')->name('reset');
+        Route::post('confirm', 'Auth\ConfirmPasswordController@confirm')->name('confirm');
+    });
+});

1e707733a2032759ce0f66c96b2f5b2e85f81d21
Farid Adam <faridibin@gmail.com>
2022-01-17 22:21:07 -0500

    Update mutations.js

--- resources/js/store/modules/auth/mutations.js
+++ resources/js/store/modules/auth/mutations.js
@@ -1,17 +1,5 @@
 export default {
-    SET: (state, payload) => {
-        const {
-            token,
-            user
-        } = payload
-
-        state.token = token
-        state.user = user
-    },
     SET_TOKEN: (state, token) => {
         state.token = token
-    },
-    SET_USER: (state, user) => {
-        state.user = user
     }
 }

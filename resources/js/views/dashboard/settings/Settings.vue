1955b70d6b8358d0017ddf5b98c072266d582fba
Farid Adam <faridibin@gmail.com>
2022-01-18 02:06:26 -0500

    Create Settings.vue

---
+++ resources/js/views/dashboard/settings/Settings.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <div class="col-lg-3 mb-3">
+            <ul class="nav nav-tabs flex-lg-column" role="tablist">
+                <li class="nav-item">
+                    <router-link :to="{ name: 'settings.profile'}" class="nav-link">Your Profile</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{ name: 'settings.password'}" class="nav-link">Password</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{ name: 'settings.notifications'}" class="nav-link">Notifications</router-link>
+                </li>
+            </ul>
+        </div>
+
+        <router-view></router-view>
+    </div>
+</template>

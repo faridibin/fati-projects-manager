a963e6c13b4036fc1eb7f65021ada63df690a097
Farid Adam <faridibin@gmail.com>
2022-01-17 22:20:18 -0500

    Update index.js

--- resources/js/store/subscriber/index.js
+++ resources/js/store/subscriber/index.js
@@ -1,5 +1,6 @@
 import store from '../index'
 import auth from './auth';
+import user from './user';
 
 const getModule = (mutationType) => {
     return mutationType.split('/')[0]
@@ -9,6 +10,9 @@ store.subscribe((mutation) => {
     switch (getModule(mutation.type)) {
         case 'auth':
             auth(mutation)
+            break
+        case 'user':
+            user(mutation)
             break
         default:
             break

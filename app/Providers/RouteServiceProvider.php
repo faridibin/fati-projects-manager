4bd578f6a041ddd22c06134d8ff83b094ef450f1
Farid Adam <faridibin@gmail.com>
2022-01-17 21:34:54 -0500

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,7 +24,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/app/dashboard';
 
     /**
      * The path to the "login" route for your application.

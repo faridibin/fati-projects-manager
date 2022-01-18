fccb8a28f7766b19bab026ee9967561b64a2e797
Farid Adam <faridibin@gmail.com>
2022-01-17 21:37:55 -0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+import store from './store'
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -6,6 +8,7 @@
 
 require('./bootstrap');
 require('./imports');
+require('./store/subscriber')
 
 window.Vue = require('vue');
 
@@ -20,14 +23,15 @@ window.Vue = require('vue');
 const files = require.context('./', true, /\.vue$/i)
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+// store.dispatch('user/attemptGet', localStorage.getItem('token')).then(() => {
 const app = new Vue({
     el: '#app',
+    store
 });
+// })

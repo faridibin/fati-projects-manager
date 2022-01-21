cd1bc5d9c0114d1b58e84a8489319c827893424d
Farid Adam <faridibin@gmail.com>
2022-01-21 00:51:23 -0500

    Update Profile.vue

--- resources/js/views/dashboard/settings/Profile.vue
+++ resources/js/views/dashboard/settings/Profile.vue
@@ -1,52 +1,46 @@
 <template>
     <div class="col-xl-8 col-lg-9">
+        <div v-if="form.successful" class="alert alert-success" role="alert">
+            Your profile has been updated.
+        </div>
         <div class="card">
             <div class="card-body">
-                <!-- <div class="media mb-4">
-                    <img alt="Image" src="assets/img/avatar-male-4.jpg" class="avatar avatar-lg" />
-                    <div class="media-body ml-3">
-                        <div class="custom-file custom-file-naked d-block mb-1">
-                            <input type="file" class="custom-file-input d-none" id="avatar-file">
-                            <label class="custom-file-label position-relative" for="avatar-file">
-                            <span class="btn btn-primary">
-                            Upload avatar
-                            </span>
-                            </label>
-                        </div>
-                        <small>For best results, use an image at least 256px by 256px in either .jpg or .png format</small>
-                    </div>
-                </div> -->
-                <!--end of avatar-->
-                <form>
+                <profile-avatar-component></profile-avatar-component>
+
+                <form @submit.prevent="save" @keydown="form.onKeydown($event)">
                     <div class="form-group row align-items-center">
                         <label class="col-3">First Name</label>
                         <div class="col">
-                            <input type="text" placeholder="First name" value="David" name="profile-first-name" class="form-control"/>
+                            <input v-model="form.first_name" type="text" placeholder="First name" class="form-control" :class="{ 'is-invalid': form.errors.has('first_name') }"/>
+                            <has-error :form="form" field="first_name"/>
                         </div>
                     </div>
                     <div class="form-group row align-items-center">
                         <label class="col-3">Last Name</label>
                         <div class="col">
-                            <input type="text" placeholder="First name" value="Whittaker" name="profile-last-name" class="form-control"/>
+                            <input v-model="form.last_name" type="text" placeholder="First name" class="form-control" :class="{ 'is-invalid': form.errors.has('last_name') }"/>
+                            <has-error :form="form" field="last_name"/>
                         </div>
                     </div>
                     <div class="form-group row align-items-center">
                         <label class="col-3">Email</label>
                         <div class="col">
-                            <input type="email" placeholder="Enter your email address" name="profile-email" class="form-control"/>
+                            <input v-model="form.email" type="email" placeholder="Enter your email address" class="form-control" :class="{ 'is-invalid': form.errors.has('email') }"/>
+                            <has-error :form="form" field="email"/>
                         </div>
                     </div>
                     <div class="form-group row align-items-center">
                         <label class="col-3">Location</label>
                         <div class="col">
-                            <input type="text" placeholder="Enter your location" name="profile-location" class="form-control"/>
+                            <input v-model="form.location" type="text" placeholder="Enter your location" class="form-control" :class="{ 'is-invalid': form.errors.has('location') }"/>
+                            <has-error :form="form" field="location"/>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-3">Bio</label>
                         <div class="col">
-                            <textarea placeholder="Tell us a little about yourself" class="form-control" rows="5"></textarea>
-                            <small>This will be displayed on your public profile</small>
+                            <textarea v-model="form.bio" placeholder="Tell us a little about yourself" class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }" rows="5"></textarea>
+                            <has-error :form="form" field="bio"/>
                         </div>
                     </div>
                     <div class="d-flex justify-content-end">
@@ -59,7 +53,38 @@
 </template>
 
 <script>
-	export default {};
+	import { mapGetters, mapActions } from "vuex";
+
+	export default {
+		data: () => ({
+			form: new Form({
+				bio: null,
+				email: null,
+				first_name: null,
+				last_name: null,
+				location: null,
+				name: null,
+			}),
+		}),
+		computed: {
+			...mapGetters({
+				user: "user/user",
+			}),
+		},
+		watch: {
+			user(data) {
+				this.form.update(data);
+			},
+		},
+		methods: {
+			...mapActions({
+				attemptUpdateUser: "user/attemptUpdateUser",
+			}),
+			save() {
+				this.attemptUpdateUser(this.form).then(() => this.form.reset());
+			},
+		},
+	};
 </script>
 
 <style>

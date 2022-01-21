00e2f655372412b63195c77fccb931c2da9475ae
Farid Adam <faridibin@gmail.com>
2022-01-21 00:27:37 -0500

    Update Password.vue

--- resources/js/views/dashboard/settings/Password.vue
+++ resources/js/views/dashboard/settings/Password.vue
@@ -1,29 +1,34 @@
 <template>
     <div class="col-xl-8 col-lg-9">
+        <div v-if="form.successful" class="alert alert-success" role="alert">
+            Your password has been changed.
+        </div>
         <div class="card">
             <div class="card-body">
-                <form>
+                <form @submit.prevent="changePasssword" @keydown="form.onKeydown($event)">
                     <div class="form-group row align-items-center">
                         <label class="col-3">Current Password</label>
                         <div class="col">
-                            <input type="password" placeholder="Enter your current password" name="password-current" class="form-control" />
+                            <input v-model="form.current_password" type="password" placeholder="Enter your current password" class="form-control" :class="{ 'is-invalid': form.errors.has('current_password') }"/>
+                            <has-error :form="form" field="current_password"/>
                         </div>
                     </div>
                     <div class="form-group row align-items-center">
                         <label class="col-3">New Password</label>
                         <div class="col">
-                            <input type="password" placeholder="Enter a new password" name="password-new" class="form-control" />
-                            <small>Password must be at least 8 characters long</small>
+                            <input v-model="form.password" type="password" placeholder="Enter a new password" class="form-control" :class="{ 'is-invalid': form.errors.has('password') }"/>
+                            <has-error :form="form" field="password"/>
                         </div>
                     </div>
                     <div class="form-group row align-items-center">
                         <label class="col-3">Confirm Password</label>
                         <div class="col">
-                            <input type="password" placeholder="Confirm your new password" name="password-new-confirm" class="form-control" />
+                            <input v-model="form.password_confirmation" type="password" placeholder="Confirm your new password" class="form-control" :class="{ 'is-invalid': form.errors.has('password_confirmation') }"/>
+                            <has-error :form="form" field="password_confirmation"/>
                         </div>
                     </div>
                     <div class="d-flex justify-content-end">
-                        <button type="submit" class="btn btn-primary">Change Password</button>
+                        <button-component>Change Password</button-component>
                     </div>
                 </form>
             </div>
@@ -32,7 +37,25 @@
 </template>
 
 <script>
-	export default {};
+	import { mapActions } from "vuex";
+
+	export default {
+		data: () => ({
+			form: new Form({
+				current_password: null,
+				password: null,
+				password_confirmation: null,
+			}),
+		}),
+		methods: {
+			...mapActions({
+				attemptChangePassword: "user/attemptChangePassword",
+			}),
+			changePasssword() {
+				this.attemptChangePassword(this.form).then(() => this.form.reset());
+			},
+		},
+	};
 </script>
 
 <style>

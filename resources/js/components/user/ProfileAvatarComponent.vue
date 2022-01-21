898819f57bdc81b62c9f255bd4dcdd26624b1cd2
Farid Adam <faridibin@gmail.com>
2022-01-21 00:51:33 -0500

    Create ProfileAvatarComponent.vue

---
+++ resources/js/components/user/ProfileAvatarComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="media mb-2">
+        <img alt="Image" src="https://s.gravatar.com/avatar/30cf50b3ac0c53c4adfab948a1fac7a0" class="avatar avatar-lg"/>
+        <div class="media-body ml-3">
+            <div class="custom-file custom-file-naked d-block mb-1">
+                <input type="file" class="custom-file-input d-none" id="avatar-file">
+                <label class="custom-file-label position-relative" for="avatar-file">
+                    <span class="btn btn-primary">Upload avatar</span>
+                </label>
+            </div>
+            <small>For best results, use an image at least 256px by 256px in either .jpg or .png format</small>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style>
+</style>

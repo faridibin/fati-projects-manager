8dd140771cf023ef33ffbbfca362cafc7e68a128
Farid Adam <faridibin@gmail.com>
2022-02-16 15:26:07 -0500

    Create TeamCardComponent.vue

---
+++ resources/js/components/ui/teams/TeamCardComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card card-team">
+        <div class="card-body">
+            <div class="dropdown card-options">
+            <button class="btn-options" type="button" id="..." data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <em class="fas fa-ellipsis-v"></em>
+            </button>
+            <div class="dropdown-menu dropdown-menu-right">
+                ...
+            </div>
+            </div>
+            <div class="card-title">
+            <a href="#"><h5>Team Name</h5></a>
+            <span># Projects, # Members</span>
+            </div>
+            <ul class="avatars">
+            ...
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style>
+</style>

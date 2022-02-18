2f62c2990ddacd20d14fddeea1d540f0651a32e5
Farid Adam <faridibin@gmail.com>
2022-02-18 14:23:10 -0500

    Update mutations.js

--- resources/js/store/modules/user/mutations.js
+++ resources/js/store/modules/user/mutations.js
@@ -2,6 +2,9 @@ export default {
     SET_USER: (state, user) => {
         state.user = user
     },
+    SET_SETTINGS: (state, settings) => {
+        state.settings = settings
+    },
     SET_AVATAR: (state, avatar) => {
         state.avatar = avatar
     }

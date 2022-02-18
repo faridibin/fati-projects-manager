d21b68e5a2b332d753f0f2fc65611af46f594f3f
Farid Adam <faridibin@gmail.com>
2022-02-18 14:23:27 -0500

    Update Notifications.vue

--- resources/js/views/dashboard/settings/Notifications.vue
+++ resources/js/views/dashboard/settings/Notifications.vue
@@ -1,21 +1,24 @@
 <template>
     <div class="col-xl-8 col-lg-9">
+        <div v-if="form.successful" class="alert alert-success" role="alert">
+            Your preferences have been saved.
+        </div>
         <div class="card">
             <div class="card-body">
-                <form>
+                <form @submit.prevent="save" @keydown="form.onKeydown($event)">
                     <h6>Activity Notifications</h6>
-                    <div v-for="() in " class="form-group">
+                    <div v-for="(setting, index) in form.activity" v-bind:key="index" class="form-group">
                         <div class="custom-control custom-checkbox custom-checkbox-switch">
-                            <input type="checkbox" class="custom-control-input" :id="'activity-'+index" checked>
-                            <label class="custom-control-label" :for="'activity-'+index">Text goes here</label>
+                            <input v-model="form.activity[index]" type="checkbox" class="custom-control-input" :id="'activity-'+index" :checked="setting">
+                            <label class="custom-control-label" :for="'activity-'+index">{{ index|trans('settings.notifications.activity') }}</label>
                         </div>
                     </div>
 
                     <h6>Service Notifications</h6>
-                    <div class="form-group">
+                    <div v-for="(setting, index) in form.service" v-bind:key="index" class="form-group">
                         <div class="custom-control custom-checkbox custom-checkbox-switch">
-                            <input type="checkbox" class="custom-control-input" id="notify-5">
-                            <label class="custom-control-label" for="notify-5">Text goes here</label>
+                            <input v-model="form.service[index]" type="checkbox" class="custom-control-input" :id="'service-'+index" :checked="setting">
+                            <label class="custom-control-label" :for="'service-'+index">{{ index|trans('settings.notifications.service') }}</label>
                         </div>
                     </div>
 
@@ -29,7 +32,43 @@
 </template>
 
 <script>
-	export default {};
+	import { mapGetters, mapActions } from "vuex";
+
+	export default {
+		mounted() {
+			this.setData(this.settings);
+		},
+		data: () => ({
+			form: new Form({
+				activity: null,
+				service: null,
+			}),
+		}),
+		computed: {
+			...mapGetters({
+				settings: "user/settings",
+			}),
+		},
+		watch: {
+			settings(data) {
+				this.setData(data);
+			},
+		},
+		methods: {
+			...mapActions({
+				attemptUpdateNotificationSettings:
+					"user/attemptUpdateNotificationSettings",
+			}),
+			setData(data) {
+				this.form.update(JSON.parse(JSON.stringify(data)));
+			},
+			save() {
+				this.attemptUpdateNotificationSettings(this.form).then(
+					() => {} // this.form.reset()
+				);
+			},
+		},
+	};
 </script>
 
 <style>
